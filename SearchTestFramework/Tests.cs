37e86f6efb5a7a5d546d97a647a4d8ec28464b84
Dmytro Filonenko <dmytro.flone@gmail.com>
2023-05-30 15:12:21 +0300

    Add necessary packages

---
+++ SearchTestFramework/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SearchTestFramework
+{
+    internal class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "https://www.google.com";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
