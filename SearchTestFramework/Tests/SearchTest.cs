2c48a4ae004960736795168fb0959d0b21605481
Dmytro Filonenko <dmytro.flone@gmail.com>
2023-06-01 16:50:27 +0300

    Remove test

--- SearchTestFramework/Tests/SearchTest.cs
+++ SearchTestFramework/Tests/SearchTest.cs
@@ -32,20 +32,5 @@ namespace SearchTestFramework.Tests.Tests
             // Assert
             Assert.That(actualResult.Contains(expectedText), $"Actual string '{actualResult}' doesn't contain expected string '{expectedText}'");
         }
-
-        [Test(Description = "Verify that an user see same search text as he entered")]
-        public void TestTextareaIsSameAsUserEntered()
-        {
-            // Arrange
-            var expectedText = "Hello World";
-
-            // Act
-            _homePage.PopulateSearchTextarea(expectedText);
-            _homePage.ClickSearchButton();
-            var actualText = _searchResultsPage.GetSearchTextareaText();
-
-            // Assert
-            Assert.AreEqual(expectedText, actualText);
-        }
     }
 }
